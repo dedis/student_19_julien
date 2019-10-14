02f1be9fbe6033684a92fb93f7aeb67d95edb164
Julien <julien.vonfelten@epfl.ch>
2019-10-14 10:49:55 +0200

    Update constants.ts with bigint

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import BN from "bn.js";
 
-export type BNType = number | string | number[] | Buffer | BN;
+export type BNType = number | string | number[] | Buffer | BN | bigint;
 
 export const zeroBN = new BN(0);
 export const oneBN = new BN(1);
