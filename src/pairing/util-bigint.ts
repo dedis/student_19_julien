aca589ffac8f7873fea5490f9aa10a43eb218f8e
Julien <julien.vonfelten@epfl.ch>
2019-10-19 11:15:55 +0200

    Implement egcd

--- src/pairing/util-bigint.ts
+++ src/pairing/util-bigint.ts
@@ -1,11 +1,34 @@
-export function egcd(e1: bigint, e2: bigint): {a:bigint, b:bigint,gcd:bigint}{
+export function egcd(m: bigint, n: bigint): {a:bigint, b:bigint,gcd:bigint}{
     //egcd between p and e1
     //can be done from https://github.com/lpcsmath/egcd/blob/master/javascript/egcd.js
+        let a1 : bigint = 1n;
+        let b1 : bigint = 0n;
+        let a : bigint  = 0n;
+        let b : bigint = 1n;
+        let c : bigint = m;
+        let d :bigint = n;
+        let q : bigint = c/d;
+        let r = c % d;
+        // such that
+        //             a1 m + b2 n = c
+        //                 am + bn = d
+        // (a1 - qa)m + (b1 - qb)n = r
+        while (r > 0) {
+            let t : bigint = a1;
+            a1 = a;
+            a = t - q*a; // a1 - qa
+            t = b1;
+            b1 = b;
+            b = t - q*b; // b1 - qb
+            c = d;
+            d = r;
+            q = c/d;
+            r = c % d;
+        }
     return {
-        a:1n,
-        b:2n,
-        gcd:3n
-    
+        a:a,
+        b:b,
+        gcd:d
     };
 
 }
