80feadecd0fda112f665d76fd9197f076da1773f
Julien <julien.vonfelten@epfl.ch>
2019-10-19 10:26:52 +0200

    Create util-bigint.ts

---
+++ src/pairing/util-bigint.ts
@@ -0,0 +1,11 @@
+export function egcd(e1: bigint, e2: bigint): {a:bigint, b:bigint,gcd:bigint}{
+    //egcd between p and e1
+    //can be done from https://github.com/lpcsmath/egcd/blob/master/javascript/egcd.js
+    return {
+        a:1n,
+        b:2n,
+        gcd:3n
+    
+    };
+
+}
