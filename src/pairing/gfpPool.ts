b32027e29fbd0fffad25e55aa51a0dcd34231e24
Julien <julien.vonfelten@epfl.ch>
2019-12-01 10:00:57 +0100

    Add all pools

--- src/pairing/gfpPool.ts
+++ src/pairing/gfpPool.ts
@@ -5,6 +5,18 @@ import GfP2 from './gfp2';
 import GfP12 from './gfp12';
 import GfP6 from './gfp6';
 
-export const GFpPool12 = deePool.create(function makeGFP12(){
+export const GfPPool1 = deePool.create(function makeGFP12(){
+        return new GfP()
+})
+
+export const GfPPool2 = deePool.create(function makeGFP12(){
+        return new GfP2(new GfP(), new GfP())
+})
+
+export const GfPPool6 = deePool.create(function makeGFP12(){
+        return new GfP6()
+})
+
+export const GfPPool12 = deePool.create(function makeGFP12(){
         return new GfP12()
 })
